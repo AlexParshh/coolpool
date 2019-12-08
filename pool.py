ab2ac27a0f9912d4227300e555ace3b6244801db
Alexparshh <alexparshh@gmail.com>
2019-12-08 17:18:02 -0500

    added image

--- pool.py
+++ pool.py
@@ -2,8 +2,20 @@ import pygame
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_captioN("cool pool")
+gameDisplay = pygame.display.set_mode((1200,800))
+pygame.display.set_caption("cool pool")
 
-print("hello")
+running = True
+
+poolImg = pygame.image.load("coolpool.png")
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    gameDisplay.fill((255,255,255))
+    gameDisplay.blit(poolImg, (500, 200))
+    pygame.display.update()
+
 
