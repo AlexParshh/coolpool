b400fc7ea7ad163db8eb11d275a6a03cd3de488d
Luca-Marinescu <luca.gmarinescu@gmail.com>
2019-12-21 18:38:10 -0500

    testing git

--- pool.py
+++ pool.py
@@ -15,7 +15,7 @@ def text_objects(text,font):
     textSurface = font.render(text,True,(0,0,0))
     return textSurface, textSurface.get_rect()
 
-#testing
+#testing luca
 def intro():
     intro = True
 
